2d09ca4104b260e26cec18035689dfebf4d5fdd7
zahodinachay <zahodinachay@gmail.com>
2020-07-24 00:10:13 +0300

    webdirvver

--- w_app_code/core/request.py
+++ w_app_code/core/request.py
@@ -4,4 +4,16 @@ from selenium import webdriver
 
 class Request:
     def __init__(self, base_url):
-        self.
+        self._phantomjs_path = os.path.join(os.curdir, 'phantomjs/bin/phantomjs')
+        self._base_url = base_url
+        self._driver = webdriver.PhantomJs(self._phantomjs_path)
+
+    def fetch_data(self, forecast, area):
+        url = self._base_url.format(forecast=forecast, area=area)
+        self._driver.get(url)
+
+        if self._driver.title == '404 Not Found':
+            error_message = ('Could not find the area that you searching for')
+            raise Exception(error_message)
+
+        return self._driver.page_source
